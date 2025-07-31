186f537a025feb5772e49a4a27edc28c0d4c9309
ildoc <il_doc@protonmail.com>
2025-07-31 12:52:53 +0200

    config

---
+++ modules/config.nix
@@ -0,0 +1,33 @@
+{ lib, ... }:
+
+{
+  options.myConfig = {
+    # User configurations
+    users = {
+      filippo = {
+        gitUserName = lib.mkOption {
+          type = lib.types.str;
+          default = "ildoc";
+          description = "Git username for filippo";
+        };
+        
+        gitUserEmail = lib.mkOption {
+          type = lib.types.str;
+          default = "il_doc@protonmail.com";
+          description = "Git email for filippo";
+        };
+      };
+    };
+    
+    # Host-specific configurations
+    hosts = {
+      slimbook = {
+        wallpaper = lib.mkOption {
+          type = lib.types.str;
+          default = "slimbook.png";
+          description = "Wallpaper filename for slimbook";
+        };
+      };
+    };
+  };
+}
